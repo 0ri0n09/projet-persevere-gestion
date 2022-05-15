76ccb7d2b4d737f8774a9db61dfd084e3f5db3bf
Orion <jojo2209@live.fr>
2022-05-15 20:32:25 +0200

    update logout

--- public/js/accueil.js
+++ public/js/accueil.js
@@ -1,7 +1,6 @@
 const logout = document.getElementById('logout');
 
 logout.addEventListener('click', () => {
-    console.log("logout");
     firebase.auth().signOut();
     window.location.href = './index.html';
 });
